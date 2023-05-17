32bfcd6acd93ab1ab0d7a6d843e20b09d11f9ae9
Juan Carlos <Juancarmgc2017@gmail.com>
2023-05-17 14:02:57 +0200

    ADICIONAL 1

--- src/PrincipalApp.java
+++ src/PrincipalApp.java
@@ -1,6 +1,33 @@
-
+/**
+ * Esta clase no tengo ni zorra de lo que hace 
+ * @author Juan Carlos
+ *
+ */
 public class PrincipalApp {
+	    private String frase;
+	    /**
+	     * 
+	     * @param frase
+	     */
+	    public void BuscaPalabra(String frase) {
+	        this.frase = frase;
+	    }
+	    /**
+	     * este es el constructor
+	     * @return
+	     */
+		public String getFrase() {
+			return frase;
+		}
 
-	int gay = 1000;
-	String clu = "Chupapenes";
-}
+		public void setFrase(String frase) {
+			this.frase = frase;
+		}
+		public boolean buscarPalabra(String palabra) {
+			/**
+			 * este es el metodo
+			 */
+			int indice = this.frase.indexOf(palabra);
+		    return indice != -1;  
+		}
+	}
